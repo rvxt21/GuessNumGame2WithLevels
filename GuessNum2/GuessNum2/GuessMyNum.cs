24c5d2015cf6bf8941a9f4882578020be8fdb07d
rvxt21 <78904227+rvxt21@users.noreply.github.com>
2021-09-17 20:07:27 +0300

    new try

--- GuessNum2/GuessNum2/GuessMyNum.cs
+++ GuessNum2/GuessNum2/GuessMyNum.cs
@@ -7,47 +7,61 @@ namespace GuessNum2
     class GuessMyNum
     {
 
-        int tries_count;
-        int winRate;
-        int[] numberOfAttempts = new int[] { 10, 7, 5 };
-        int value;
+       protected int tries_count;
+       protected int winRate;
+       protected int[] numberOfAttempts = new int[] { };
+       protected  int value;
+       protected int numlevels;
+       protected int numAttempts;
         public GuessMyNum() 
         {
             Console.WriteLine("Start game!");
             Random rnd = new Random();
             value = rnd.Next(0, 100);
         }
-
         public void IsItRight()
         {
-            Console.WriteLine("Enter your num:\n");
-            int yourNum;
-            int j = 0;
-            for (int i = 0; i < numberOfAttempts[j]; i++) 
+            Console.WriteLine("Enter number levels:\n");
+            numlevels = Convert.ToInt32(Console.ReadLine());
+            int[] numberOfAttempts = new int[numlevels];
+            for (int i = 0; i < numberOfAttempts.Length; i++)
             {
-                yourNum = Convert.ToInt32(Console.ReadLine());
-                if (yourNum > value)
+                Console.WriteLine($"Enter num of attempts on {i + 1} level:\n");
+                numberOfAttempts[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int yourNum;
+            for (int i=0;i< numberOfAttempts.Length; i++)
+            {
+                Console.WriteLine($"This is {i + 1} level!\nEnter your num to guess:\n");
+                for (int j = 0; j <= numberOfAttempts[i]; j++)
                 {
-                    Console.WriteLine("Your num is too high.Try again!\n");
-                }
-                else if (yourNum < value)
-                {
-                    Console.WriteLine("Your num is too small.Try again!\n");
-                }
-                else
-                {
-                    if (j != 2) {
-                        Console.WriteLine("You win this level!\n");
+                    yourNum = Convert.ToInt32(Console.ReadLine());
+                    if  (yourNum > value)
+                        {
+                            Console.WriteLine("Your num is too high.Try again!\n");
+                        }
+                        else if (yourNum < value)
+                        {
+                            Console.WriteLine("Your num is too small.Try again!\n");
+                        }
+                        else
+                        {
+                        if (i != numberOfAttempts.Length)
+                        {
+                            Console.WriteLine("You win this level!!\n");
+                            winRate++;
+                        }
+                        else
+                        {
+                            Console.WriteLine("You win game!!\n");
+                        }
+                        Random rnd = new Random();
+                        value = rnd.Next(0, 100);
                     }
-                    else if (j == 2)
-                    {
-                        Console.WriteLine("You win this game!!");
-                    }
-                    j++;
-                    Random rnd = new Random();
-                    value = rnd.Next(0, 100);
+                   
                 }
             }
+            
         }
     }
 }
