4631e67443baf7008faeb9e90235d86ebcf43643
rvxt21 <78904227+rvxt21@users.noreply.github.com>
2021-09-17 12:40:28 +0300

    1

---
+++ GuessNum2/GuessNum2/GuessMyNum.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GuessNum2
+{
+    class GuessMyNum
+    {
+
+        int tries_count;
+        int winRate;
+        int[] numberOfAttempts = new int[] { 10, 7, 5 };
+        int value;
+        public GuessMyNum() 
+        {
+            Console.WriteLine("Start game!");
+            Random rnd = new Random();
+            value = rnd.Next(0, 100);
+        }
+
+        public void IsItRight()
+        {
+            Console.WriteLine("Enter your num:\n");
+            int yourNum;
+            int j = 0;
+            for (int i = 0; i < numberOfAttempts[j]; i++) 
+            {
+                yourNum = Convert.ToInt32(Console.ReadLine());
+                if (yourNum > value)
+                {
+                    Console.WriteLine("Your num is too high.Try again!\n");
+                }
+                else if (yourNum < value)
+                {
+                    Console.WriteLine("Your num is too small.Try again!\n");
+                }
+                else
+                {
+                    if (j != 2) {
+                        Console.WriteLine("You win this level!\n");
+                    }
+                    else if (j == 2)
+                    {
+                        Console.WriteLine("You win this game!!");
+                    }
+                    j++;
+                    Random rnd = new Random();
+                    value = rnd.Next(0, 100);
+                }
+            }
+        }
+    }
+}
---
+++ GuessNum2/GuessNum2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GuessNum2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessMyNum a = new GuessMyNum();
+            a.IsItRight();
+            Console.ReadLine();
+        }
+    }
+}
